a5ee1d2413f2f03d3a90b12a20a7aeaea3f1dd5a
ppoak <oakery@qq.com>
2024-01-19 13:16:28 +0800

    published v0.1.0

---
+++ README.md
@@ -0,0 +1,15 @@
+# Factor
+
+This is a framework for factor mining and backtesting. The data supply is from anthor quantative tool repository named `quool`, see [quool](https://github.com/ppoak/quool) for more information.
+
+## Usage
+
+You can simple click download file in zip format on github, or you can use git command line if you want:
+
+```
+git clone git@github.com:ppoak/factor
+```
+
+## Change Log
+
+version 0.1.0: current version. add `report` module can help you backtesting any factor you want. Organized some computing modules named `barra`, `financial`, `highfreq`.
---
+++ highfreq.py
@@ -0,0 +1,38 @@
+"""
+This script is used to compute high frequency factors:
+
+- tail volume: the total volume in tail 3-minutes
+"""
+
+# %% here is to import some packages
+import pandas as pd
+from tqdm import tqdm
+from joblib import Parallel, delayed
+from quool.table import PanelTable
+
+# %% here is to create some interfaces
+start = "20000101"
+stop = None
+
+qtm = PanelTable("/home/data/quotes-min")
+qtd = PanelTable('/home/data/quotes-day')
+fin = PanelTable("/home/data/financial")
+fctdev = PanelTable("/home/data/factordev")
+
+# %% get trading days for data is too large, can only be computed daily
+trading_days = qtd.read('close', code='000001.XSHE').index.get_level_values('date')
+
+# %% tail volume factor computation
+@delayed
+def _get_tailvolume(_cpd):
+    _data = qtm.read('volume', start=_cpd, stop=_cpd + pd.Timedelta(days=1)).iloc[:, 0]
+    _factor = _data.unstack(level='order_book_id').between_time("14:57", "15:00").sum(axis=0)
+    return _factor
+
+computing_days = trading_days[4359:]
+tailvolume3min = Parallel(n_jobs=-1, backend='loky')(_get_tailvolume(cpd) for cpd in tqdm(computing_days))
+tailvolume3min = pd.concat(tailvolume3min, axis=0, keys=computing_days).reorder_levels(['order_book_id', 'date'])
+
+# %% 
+tailvolume3min.name = 'tailvolume3min'
+fctdev.add(tailvolume3min) if 'tailvolume3min' in fctdev.columns else fctdev.add(tailvolume3min)
