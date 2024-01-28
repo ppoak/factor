c6a0467d61b1d241411be0b5155417d945462918
ppoak <oakery@qq.com>
2024-01-28 11:14:12 +0800

    gather uri to constant

--- highfreq.py
+++ highfreq.py
@@ -4,17 +4,19 @@ from tqdm import tqdm
 from joblib import Parallel, delayed
 
 
+QTM_URI = '/home/data/quotes-min'
+QTD_URI = '/home/data/quotes-day'
+
+
 def get_tail_volume_percent(
     start: str, stop: str,
 ) -> pd.DataFrame:
 
     def _get_tvp(td):
-        _data = ft.get_data(qtm_uri, "volume", start=td, stop=td + pd.Timedelta(days=1))
+        _data = ft.get_data(QTM_URI, "volume", start=td, stop=td + pd.Timedelta(days=1))
         return _data.between_time("14:30", "15:00").sum(axis=0) / _data.sum(axis=0)
         
-    qtm_uri = '/home/data/quotes-min'
-    qtd_uri = '/home/data/quotes-day'
-    trading_days = ft.get_trading_days(qtd_uri, start, stop)
+    trading_days = ft.get_trading_days(QTD_URI, start, stop)
     return -pd.concat(Parallel(n_jobs=-1, backend='loky')
         (delayed(_get_tvp)(td) for td in tqdm(trading_days)),
         axis=0, keys=trading_days
