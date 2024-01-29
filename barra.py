8aa2db13462ca7de452b434975d814e2f781ca97
ppoak <oakery@qq.com>
2024-01-29 13:40:31 +0800

    add some constant, add volatility factor

--- barra.py
+++ barra.py
@@ -5,6 +5,7 @@ Referring to BARRA-USE5, more explaination at: https://zhuanlan.zhihu.com/p/3141
 import numpy as np
 import factor as ft
 import pandas as pd
+import statsmodels.api as sm
 from joblib import Parallel, delayed
 
 
@@ -12,54 +13,86 @@ QTD_URI = '/home/data/quotes-day'
 IDXQTD_URI = '/home/data/index-quotes-day'
 FIN_URI = '/home/data/financial'
 
+YEAR = 252
+MONTH = 21
 
-def get_logsize(
-    start: str, stop: str,
-) -> pd.DataFrame:
+INDEX_CODE = "000985.XSHG"
+
+
+def get_logsize(start: str, stop: str) -> pd.DataFrame:
     shares = ft.get_data(QTD_URI, "circulation_a", start=start, stop=stop)
     price = ft.get_data(QTD_URI, "close", start=start, stop=stop)
     adjfactor = ft.get_data(QTD_URI, "adjfactor", start=start, stop=stop)
     return -np.log(shares * price * adjfactor).loc[start:stop]
 
 def get_beta(start: str, stop: str) -> pd.DataFrame:
-    rollback = ft.get_trading_days_rollback(QTD_URI, start, 253)
+    rollback = ft.get_trading_days_rollback(QTD_URI, start, YEAR + 1)
     price = ft.get_data(QTD_URI, "close", start=rollback, stop=stop)
-    index_price = ft.get_data(IDXQTD_URI, "close", pool="000985.XSHG", start=rollback, stop=stop).squeeze()
+    index_price = ft.get_data(IDXQTD_URI, "close", pool=INDEX_CODE, start=rollback, stop=stop).squeeze()
     adjfactor = ft.get_data(QTD_URI, "adjfactor", start=rollback, stop=stop)
     price *= adjfactor
     returns = price.pct_change(fill_method=None).iloc[1:]
     index_returns = index_price.pct_change(fill_method=None).iloc[1:]
 
     def _g_b(x):
-        if x.shape[0] < 252:
+        if x.shape[0] < YEAR:
             return pd.Series(index=x.columns)
         idxrt = index_returns.loc[x.index]
         return x.apply(lambda y:
-            ((y - y.mean()) * (idxrt - idxrt.mean())).ewm(halflife=63).mean().iloc[-1] /
-            ((idxrt - idxrt.mean()) ** 2).ewm(halflife=63).mean().iloc[-1]
+            ((y - y.mean()) * (idxrt - idxrt.mean())).ewm(halflife=YEAR // 2).mean().iloc[-1] /
+            ((idxrt - idxrt.mean()) ** 2).ewm(halflife=YEAR // 2).mean().iloc[-1]
         )
     
-    beta = Parallel(n_jobs=-1, backend="loky")(delayed(_g_b)(x) for x in returns.rolling(252))
-    return pd.concat(beta[252:], axis=1, keys=returns.index[252:]).T.loc[start:stop]
+    beta = Parallel(n_jobs=-1, backend="loky")(delayed(_g_b)(x) for x in returns.rolling(YEAR))
+    return pd.concat(beta[YEAR:], axis=1, keys=returns.index[YEAR:]).T.loc[start:stop]
 
-def get_momentum_20d(
-    start: str, stop: str,
-) -> pd.DataFrame:
-    rollback = ft.get_trading_days_rollback(QTD_URI, start, 21)
+def get_momentum(start: str, stop: str) -> pd.DataFrame:
+    rollback = ft.get_trading_days_rollback(QTD_URI, start, 2 * YEAR + 1)
     price = ft.get_data(QTD_URI, "close", start=rollback, stop=stop)
     adjfactor = ft.get_data(QTD_URI, "adjfactor", start=rollback, stop=stop)
     price *= adjfactor
-    return -(price / price.shift(20) - 1).loc[start:stop]
+    logreturns = np.log(price / price.shift(1)).iloc[1:]
+    def _g_m(x):
+        if x.shape[0] < 2 * YEAR:
+            return pd.Series(index=x.columns, name=x.index[-1])
+        return x.shift(MONTH).ewm(halflife=YEAR // 2).mean().iloc[-1]
+    momentum = Parallel(n_jobs=-1, backend="loky")(delayed(_g_m)(x) for x in logreturns.rolling(2 * YEAR))
+    return pd.concat(momentum, axis=1).T.loc[start:stop]
 
-def get_volatility_20d(
-    start: str, stop: str,
-) -> pd.DataFrame:
-    rollback = ft.get_trading_days_rollback(QTD_URI, start, 22)
+def get_volatility(start: str, stop: str) -> pd.DataFrame:
+    rollback = ft.get_trading_days_rollback(QTD_URI, start, YEAR + 1)
     price = ft.get_data(QTD_URI, "close", start=rollback, stop=stop)
+    index_price = ft.get_data(IDXQTD_URI, "close", pool=INDEX_CODE, start=rollback, stop=stop).squeeze()
     adjfactor = ft.get_data(QTD_URI, "adjfactor", start=rollback, stop=stop)
     price *= adjfactor
-    returns = price / price.shift(1) - 1
-    return -returns.rolling(20).std().loc[start:stop]
+    logreturns = np.log(price / price.shift(1)).iloc[1:]
+    returns = price.pct_change(fill_method=None).iloc[1:]
+    index_returns = index_price.pct_change(fill_method=None).iloc[1:]
+    beta = get_beta(rollback, stop)
+    size = get_logsize(rollback, stop)
+    datsd = logreturns.ewm(halflife=2 * MONTH).std()
+    datsd = ft.zscore(datsd)
+    cmra = Parallel(n_jobs=-1, backend="loky")(delayed(lambda x: x.cumsum().max() - x.cumsum().min())(x) for x in logreturns.rolling(YEAR))
+    cmra = pd.concat(cmra, axis=1, keys=logreturns.index).T
+    cmra = ft.zscore(cmra)
+    hsigma = (returns - beta.mul(index_returns, axis=0)).rolling(YEAR).std()
+    def _decolinear(x, y):
+        x = x.dropna()
+        y = y.dropna()
+        idx = x.index.intersection(y.index)
+        x = x.loc[idx]
+        y = y.loc[idx]
+        if x.empty or y.empty:
+            return pd.Series(index=y.index, name=y.name)
+        model = sm.OLS(y, sm.add_constant(x)).fit()
+        resid = model.resid
+        resid.name = y.name
+        return resid
+    hsigma = Parallel(n_jobs=-1, backend="loky")(delayed(_decolinear)
+        (pd.concat([size.iloc[i], beta.iloc[i]], axis=1, keys=["size", "beta"]), hsigma.iloc[i]) for i in range(len(hsigma))
+    )
+    hsigma = ft.zscore(hsigma)
+    return (0.74 * datsd + 0.16 * cmra + 0.1 * hsigma).loc[start:stop]
 
 def get_ep(
     start: str, stop: str,
