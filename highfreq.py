867825601b6c1f1d0daba3bcf584604a3bb73bd6
ppoak <oakery@qq.com>
2024-02-01 21:06:39 +0800

    add realized skew factor

--- highfreq.py
+++ highfreq.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import factor as ft
 from tqdm import tqdm
@@ -7,6 +8,7 @@ from joblib import Parallel, delayed
 QTM_URI = '/home/data/quotes-min'
 QTD_URI = '/home/data/quotes-day'
 
+MIN = 240
 WEEK = 5
 MONTH = 21
 YEAR = 252
@@ -22,6 +24,22 @@ def get_tail_volume_percent(start: str, stop: str) -> pd.DataFrame:
         result.name = pd.to_datetime(_data.index[-1].strftime('%Y%m%d'))
         return result
         
+    rollback = ft.get_trading_days_rollback(QTD_URI, start, WEEK)
+    trading_days = ft.get_trading_days(QTD_URI, rollback, stop)
+    return pd.concat(Parallel(n_jobs=-1, backend='loky')(delayed(_get)
+        (_start, _stop) for _start, _stop in  tqdm(list(
+            zip(trading_days[:-WEEK + 1], trading_days[WEEK - 1:])
+        ))), axis=1).T.loc[start:stop]
+
+def get_realized_skew(start: str, stop: str) -> pd.DataFrame:
+    def _get(_start, _stop):
+        _data = ft.get_data(QTM_URI, "close", start=_start, stop=_stop + pd.Timedelta(days=1))
+        _return = _data.pct_change(fill_method=None)
+        _return2 = _return ** 2
+        _return3 = _return ** 3
+        result = _return3.resample('d').sum() / (_return2.resample('d').sum() ** 1.5)
+        return np.sqrt(MIN - 1) / WEEK * result.sum()
+
     rollback = ft.get_trading_days_rollback(QTD_URI, start, WEEK)
     trading_days = ft.get_trading_days(QTD_URI, rollback, stop)
     return pd.concat(Parallel(n_jobs=-1, backend='loky')(delayed(_get)
