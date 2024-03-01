f1ba08ae3111d66cd7ed9f3b9d78f7e3e9c3f45f
ppoak <oakery@qq.com>
2024-03-02 00:15:28 +0800

    remove get_spot_data and format_code

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,6 @@
 import quool
 import factor
-import random
-import requests
 import datetime
-import numpy as np
-import pandas as pd
-from pathlib import Path
-from copy import deepcopy
 from IPython.core.magic import (magics_class, line_magic, Magics)
 from IPython.terminal.prompts import Prompts, Token
 
@@ -154,7 +148,7 @@ class FetcherMagics(Magics):
 
     @line_magic
     def spot(self, line):
-        self.shell.user_ns['spot'] = get_spot_data(
+        self.shell.user_ns['spot'] = quool.get_spot_data(
             self.proxies[['http']].to_dict(orient='records'))
         return self.shell.user_ns['spot']
 
@@ -174,91 +168,3 @@ class FactorPrompt(Prompts):
 def load_ipython_extension(ipython):
     ipython.register_magics(FactorMagics)
     ipython.register_magics(FetcherMagics)
-
-def proxy_request(url: str, proxies: list = None, **kwargs):
-    if not isinstance(proxies, list):
-        proxies = [proxies]
-    proxies = deepcopy(proxies)
-    while len(proxies):
-        proxy = proxies.pop(random.randint(0, len(proxies) - 1))
-        try:
-            return requests.get(url, proxies=proxy, **kwargs)
-        except:
-            continue
-    raise ConnectionError("request failed")
-
-def format_code(code, format="{code}.{market}", style: str = "wind", upper: bool = False):
-    if code.startswith("6"):
-        if upper:
-            return format.format(code=code, market="SH" if style == "wind" else "XSHG")
-        else:
-            return format.format(code=code, market="sh" if style == "wind" else "xshg")
-    elif code.startswith("3") or code.startswith("0"):
-        if upper:
-            return format.format(code=code, market="SZ" if style == "wind" else "XSHE")
-        else:
-            return format.format(code=code, market="sz" if style == "wind" else "xshe")
-    else:
-        return np.nan
-
-def get_spot_data(proxies: list = None) -> pd.DataFrame:
-    url = "http://82.push2.eastmoney.com/api/qt/clist/get"
-    params = {
-        "pn": "1",
-        "pz": "50000",
-        "po": "1",
-        "np": "1",
-        "ut": "bd1d9ddb04089700cf9c27f6f7426281",
-        "fltt": "2",
-        "invt": "2",
-        "fid": "f3",
-        "fs": "m:0 t:6,m:0 t:80,m:1 t:2,m:1 t:23,m:0 t:81 s:2048",
-        "fields": "f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,f23,f24,f25,f22,f11,f62,f128,f136,f115,f152",
-        "_": "1623833739532",
-    }
-    r = proxy_request(url, proxies, params=params)
-    data_json = r.json()
-    if not data_json["data"]["diff"]:
-        return pd.DataFrame()
-    temp_df = pd.DataFrame(data_json["data"]["diff"])
-    temp_df.columns = [
-        "_",
-        "latest_price",
-        "change_rate",
-        "change_amount",
-        "volume",
-        "turnover",
-        "amplitude",
-        "turnover_rate",
-        "pe_ratio_dynamic",
-        "volume_ratio",
-        "five_minute_change",
-        "code",
-        "_",
-        "name",
-        "highest",
-        "lowest",
-        "open",
-        "previous_close",
-        "market_cap",
-        "circulating_market_cap",
-        "speed_of_increase",
-        "pb_ratio",
-        "sixty_day_change_rate",
-        "year_to_date_change_rate",
-        "-",
-        "-",
-        "-",
-        "-",
-        "-",
-        "-",
-        "-",
-    ]
-    
-    temp_df["code"] = temp_df["code"].map(lambda x: format_code(x, format='{code}.{market}', style='ricequant', upper=True))
-    temp_df = temp_df.dropna(subset=["code"]).set_index("code")
-    temp_df = temp_df.drop(["-", "_"], axis=1)
-    for col in temp_df.columns:
-        if col != 'name':
-            temp_df[col] = pd.to_numeric(temp_df[col], errors='coerce')
-    return temp_df
