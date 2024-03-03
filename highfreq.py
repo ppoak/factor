b549996309ebc990a0fc89339c354d0b43b1a525
ppoak <oakery@qq.com>
2024-03-03 13:27:54 +0800

    fix problems in `tail_volume_percent`

--- highfreq.py
+++ highfreq.py
@@ -1,3 +1,4 @@
+import quool
 import numpy as np
 import pandas as pd
 import factor as ft
@@ -13,39 +14,37 @@ WEEK = 5
 MONTH = 21
 YEAR = 252
 DATE_LEVEL = 'datetime'
+CODE_LEVEL = 'orderbook_id'
 
 
 def get_tail_volume_percent(start: str, stop: str) -> pd.DataFrame:
-    def _get(_start, _stop):
-        _data = ft.get_data(QTM_URI, "volume", start=_start, stop=_stop + pd.Timedelta(days=1))
-        _tail_vol = _data.between_time("14:30", "15:00").resample('d').sum()
-        _day_vol = _data.resample('d').sum()
-        _res = (_tail_vol / _day_vol).astype('float').mean()
-        _res.name = pd.to_datetime(_data.index[-1].strftime('%Y%m%d'))
-        return _res
+    def _get(date: pd.Timestamp):
+        data = ft.get_data(QTM_URI, "volume", start=date, stop=date + pd.Timedelta(days=1))
+        tail_vol = data.between_time("14:30", "14:57")
+        day_vol = data.sum()
+        res = tail_vol / day_vol
+        res.name = date
+        return res
         
-    rollback = ft.get_trading_days_rollback(QTD_URI, start, WEEK)
-    trading_days = ft.get_trading_days(QTD_URI, rollback, stop)
-    return pd.concat(Parallel(n_jobs=-1, backend='loky')(delayed(_get)
-        (_start, _stop) for _start, _stop in  tqdm(list(
-            zip(trading_days[:-WEEK + 1], trading_days[WEEK - 1:])
-        ))), axis=1).T.loc[start:stop]
+    trading_days = ft.get_trading_days(QTD_URI, start, stop)
+    return pd.concat(Parallel(n_jobs=-1, backend='loky')(
+        delayed(_get)(date) for date in tqdm(list(trading_days))
+    ), axis=1).T.loc[start:stop]
 
-def get_interday_distribution(start: str, stop: str) -> pd.DataFrame:
-    def _get(_date):
-        _data = ft.get_data(QTM_URI, "close", start=_date, stop=_date + pd.Timedelta(days=1))
-        _return = _data.pct_change(fill_method=None)
-        res = pd.concat([_return.skew(), _return.kurt()], axis=1, keys=['skew', 'kurt'])
+def get_intraday_distribution(start: str, stop: str) -> pd.DataFrame:
+    def _get(date: pd.Timestamp):
+        data = ft.get_data(QTM_URI, "close", start=date, stop=date + pd.Timedelta(days=1))
+        ret = data.pct_change(fill_method=None)
+        res = pd.concat([ret.skew(), ret.kurt()], axis=1, 
+            keys=['intraday_return_skew', 'intraday_return_kurt'])
         res.index = pd.MultiIndex.from_product([
-             res.index, [_date],
-        ], names=["order_book_id", "date"])
+             res.index, [date]], names=["order_book_id", "date"])
         return res
     
     trading_days = ft.get_trading_days(QTD_URI, start, stop)
-    result = pd.concat(Parallel(n_jobs=-1, backend='loky')(delayed(_get)
-        (date) for date in  tqdm(list(trading_days))), axis=0)
-    result = result.sort_index().loc(axis=0)[:, start:stop]
-    return result
+    return pd.concat(Parallel(n_jobs=-1, backend='loky')(
+        delayed(_get)(date) for date in tqdm(list(trading_days))
+    ), axis=1).T.loc[start:stop]
 
 def get_long_short_ratio(start: str, stop: str):
     def _get(_start, _stop):
@@ -108,3 +107,35 @@ def get_regret(start: str, stop: str) -> pd.DataFrame:
     return pd.concat(Parallel(n_jobs=1, backend='loky')(delayed(_get)
         (date) for date in  tqdm(list(trading_days))), axis=0
     ).sort_index().loc(axis=0)[:, start:stop]
+
+def get_updown_volume(start: str, stop: str) -> pd.DataFrame:
+    def _get(_date):
+        _price = ft.get_data(QTM_URI, "close", start=_date, stop=_date + pd.Timedelta(days=1))
+        _vol = ft.get_data(QTM_URI, "volume", start=_date, stop=_date + pd.Timedelta(days=1))
+        _return = _price.pct_change(fill_method=None)
+        upvol = _vol[_return > 0].sum() / _vol.sum()
+        downvol = _vol[_return < 0].sum() / _vol.sum()
+        res = pd.concat([upvol, downvol], axis=1, keys=["upvol", "downvol"])
+        res.index = pd.MultiIndex.from_product([res.index, [_date]], names=["order_book_id", "date"])
+        return res
+
+    trading_days = ft.get_trading_days(QTD_URI, start, stop)
+    return pd.concat(Parallel(n_jobs=1, backend='loky')(delayed(_get)
+        (date) for date in tqdm(list(trading_days))), axis=0
+    ).sort_index().loc(axis=0)[:, start:stop]
+
+def get_average_relative_price_percent(start: str, stop: str) -> pd.DataFrame:
+    def _get(_date):
+        qtm = quool.PanelTable(QTM_URI)
+        df = qtm.read("open, high, low, close", start=_date, stop=_date + pd.Timedelta(days=1))
+        twap = df.mean(axis=1).groupby(level='order_book_id').mean()
+        high = df["high"].groupby(level='order_book_id').max()
+        low = df["low"].groupby(level='order_book_id').min()
+        arrp = (twap - low) / (high - low)
+        arrp.name = _date
+        return arrp
+
+    trading_days = ft.get_trading_days(QTD_URI, start, stop)
+    return pd.concat(Parallel(n_jobs=1, backend='loky')(delayed(_get)
+        (date) for date in tqdm(list(trading_days))), axis=1
+    ).sort_index().T.loc[:, start:stop]
